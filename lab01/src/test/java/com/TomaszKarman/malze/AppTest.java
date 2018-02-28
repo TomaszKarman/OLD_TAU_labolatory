849c40de466b03ab9287295110ec0625d277de88
TomaszKarman <s14642@pjwstk.edu.pl>
2018-02-28 18:48:58 +0000

    test class changed

--- lab01/src/test/java/com/TomaszKarman/malze/AppTest.java
+++ lab01/src/test/java/com/TomaszKarman/malze/AppTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 class CheckMalzaTest {
     @Test
     public void testCheckMalza() {
-        Malza malza1 = new Malza("Czesiu",3);
-        Malza malza2 = new Malza("Zbychu", 7);
-        ArrayList<Malza> malze = new ArrayList<Malza>();
+        public Malza malza1 = new Malza("Czesiu",3);
+        public Malza malza2 = new Malza("Zbychu", 7);
+        public ArrayList<Malza> malze = new ArrayList<Malza>();
         malze.add(malza1);
         malze.add(malza2);
 
         Integer quantity = malze.size();
-        Enum status = malza2.getStatus();
+        public Enum status = malza2.getStatus();
         assertNotNull(malza2);
         assertEquals(status, Malza.Status.Dead);
         assertEquals(quantity, 2);
