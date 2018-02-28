5040661c831607e659893d188119294840195466
TomaszKarman <s14642@pjwstk.edu.pl>
2018-02-28 18:38:09 +0000

    test class changed

--- lab01/src/test/java/com/TomaszKarman/malze/AppTest.java
+++ lab01/src/test/java/com/TomaszKarman/malze/AppTest.java
@@ -9,13 +9,13 @@ class CheckMalzaTest {
     public void testCheckMalza() {
         Malza malza1 = new Malza("Czesiu",3);
         Malza malza2 = new Malza("Zbychu", 7);
-        List<Malza> malze = new ArrayList<Malza>();
+        ArrayList<Malza> malze = new ArrayList<Malza>();
         malze.add(malza1);
         malze.add(malza2);
 
         Integer quantity = malze.size();
         //Enum status = malza2.getStatus();
-        assertNotNull(malze);
+        assertNotNull(malza2);
         //assertEquals(status, Malza.Status.Dead);
         assertEquals(quantity, 2);
     }
